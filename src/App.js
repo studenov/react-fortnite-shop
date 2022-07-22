2f313ab083ba717808f845780e6f4606c335b61a
Aleksey Studenov <aleksey.studenov@smissltd.com>
2022-07-22 17:22:09 +0300

    components, config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import {Header} from './components/Header'
+import {Main} from './components/Main'
+import {Footer} from './components/Footer'
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="green darken-1">
+        <div className="nav-wrapper">
+            <a href="#" className="brand-logo">React Shop</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="https://github.com/studenov/react-fortnite-shop">Repo</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
